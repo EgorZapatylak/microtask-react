d4db524e187139d298ec1c41d309eed1bd2c7bb9
EgorZapatylak <zapatylak96a@gmail.com>
2022-03-10 08:41:11 +0300

    modified App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Body} from './site/Body';
+import {Footer} from './site/Footer';
+import {Header} from "./site/Header";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+    return (
+        <>
+            <Header title={'NEW HEADER'}/>
+            <Body titleForBody={'NEW BODY'}/>
+            <Footer titleForFooter={'NEW FOOTER'}/>
+            
+        </>
+    );
 }
 
 export default App;
