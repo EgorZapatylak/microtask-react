8ea712b006623d05a9778da4a212e5a127f4dea1
EgorZapatylak <zapatylak96a@gmail.com>
2022-03-10 09:06:39 +0300

    modified App.tsx file for microtask-3

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
 import React, {useState} from 'react';
 import './App.css';
 
-import {NewComponent} from "./methodMap/NewComponent";
+import {Button} from "./Button/Button";
 
 
 function App() {
 
-
-
-    const topCars = [
-        {manufacturer: 'BMW', model: 'm5cs'},
-        {manufacturer: 'Mercedes', model: 'e63s'},
-        {manufacturer: 'Audi', model: 'rs6'}
+    const students = [
+        {id: 1, name: 'James', age: 8},
+        {id: 2, name: 'Robert', age: 18},
+        {id: 3, name: 'John', age: 28},
+        {id: 4, name: 'Michael', age: 38},
+        {id: 5, name: 'William', age: 48},
+        {id: 6, name: 'David', age: 58},
+        {id: 7, name: 'Richard', age: 68},
+        {id: 8, name: 'Joseph', age: 78},
+        {id: 9, name: 'Tomas', age: 88},
+        {id: 10, name: 'Charles', age: 98},
+        {id: 11, name: 'Christopher', age: 108}
     ]
 
 
 
+    const Button1foo = (name:string, age: number, adrres: string) => {
+        console.log(name,age,adrres)
+    }
+
+    const Button2foo = (name:string) => {
+        console.log(name)
+    }
+
+    const Button3foo = () => {
+        console.log('Im stupid Button')
+
+    }
+
 
     return (
         <>
-
-            <NewComponent topCars={topCars}/>
+           
+            <Button name={'My Button-1'} callBack={() => Button1foo('Im VASYA', 21, 'live in Minsk')}/>
+            <Button name={'My Button-2'} callBack={() => Button2foo ('Im IVAN')}/>
+            <Button name={'Stupid Button'} callBack={Button3foo}/>
 
         </>
     );
