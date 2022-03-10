2bdf77038ea9f57b2b02eee9b809a1c9a39117d5
EgorZapatylak <zapatylak96a@gmail.com>
2022-03-10 09:07:29 +0300

    Add file for microtask-3

---
+++ src/Button/Button.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+type ButtonType = {
+    name: string
+    callBack: () => void
+}
+
+export const Button = (props:ButtonType) => {
+
+    const onClickHandler = () => {
+        props.callBack();
+    }
+
+    return (
+        <>
+            <button onClick={onClickHandler}>{props.name}</button>
+        </>
+    )
+}
